6a2ffc9ebca74d5af4714e304f433ea5ed01a769
Mate Kovacs <kovacskmate@gmail.com>
2020-11-20 10:38:15 +0100

    user input to db is now safe

--- quizia.py
+++ quizia.py
@@ -54,7 +54,7 @@ def createDailyChallenge():
         db.commit()
         return str(questionIDs)
 
-#should only be checked at midnight in a real environment
+#should only be checked at midnight
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=checkForAchievements, trigger="interval", seconds=10)
 scheduler.add_job(func=createDailyChallenge, trigger="interval", seconds=10)
@@ -94,20 +94,6 @@ class User(UserMixin, db.Model):
     introduction = db.Column(db.String(300))
     reg_date = db.Column(db.Date)
     challenge_score = db.Column(db.Integer)
-
-
-#delete:
-@app.route('/jinjaParent')
-@login_required
-def jinjaParent():   
-    return render_template('jinjaParent.html'), 404
-
-@app.route('/jinjaChild')
-@login_required
-def jinjaChild():   
-    return render_template('jinjaChild.html'), 404
-#delete
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -163,28 +149,25 @@ def listcategories():
     categoryIDs = db.cursor().execute(sql).fetchall()
     for item in categoryIDs:
         page.append(str(item))
-    # for row in db.cursor().execute(sql):
-    #     page.append(str(row)) 
     return ''.join('b"\'\xb6\x0c\x06\xf3\xe8\x9do\xb4\xfb\xffx\x12\xafQ!\x8e\xd7ew)\x1a\x0b\x81"')
 
-#if id < 0 or id > table length return 404
 @app.route('/play/<quiz_id>')
 @login_required
 def play(quiz_id):
     db = get_db() 
     cur = db.cursor()  
-    sql = "SELECT COUNT(*) FROM quizzes WHERE quiz_id = " + quiz_id + ""
-    result = cur.execute(sql)
+    sql = "SELECT COUNT(*) FROM quizzes WHERE quiz_id = ?"
+    result = cur.execute(sql, (quiz_id, ))
     rowcount = result.fetchall()
     if rowcount[0][0] > 0:
         db = get_db() 
         cur = db.cursor()  
-        sql = "UPDATE quizzes SET plays = plays + 1 WHERE quiz_id = {}"
-        cur.execute(sql.format(quiz_id))
+        sql = "UPDATE quizzes SET plays = plays + 1 WHERE quiz_id = ?"
+        cur.execute(sql, (quiz_id, ))
         db.commit()        
-        sql = "SELECT qu.quiz_name, u.username, qs.question_id, qs.quiz_id, qs.question, qs.time_limit, qs.image, qs.answer1, qs.answer2, qs.answer3, qs.answer4, qs.correctAnswer, qs.attempts, qs.correct_attempts FROM questions qs JOIN quizzes qu ON qs.quiz_id = qu.quiz_id JOIN user u ON qu.user_id = u.id WHERE qs.quiz_id = {}"
+        sql = "SELECT qu.quiz_name, u.username, qs.question_id, qs.quiz_id, qs.question, qs.time_limit, qs.image, qs.answer1, qs.answer2, qs.answer3, qs.answer4, qs.correctAnswer, qs.attempts, qs.correct_attempts FROM questions qs JOIN quizzes qu ON qs.quiz_id = qu.quiz_id JOIN user u ON qu.user_id = u.id WHERE qs.quiz_id = ?"
         questions = []    
-        cur.execute(sql.format(quiz_id))
+        cur.execute(sql, (quiz_id, ))
         #idea from https://stackoverflow.com/questions/3286525/return-sql-table-as-json-in-python
         r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
         userid = current_user.id
@@ -220,7 +203,7 @@ def paging(page, sql, template):
     cur = db.cursor()  
     sql = sql
     startLimit = int(page) * 20 - 20
-    result = cur.execute(sql.format(startLimit,21))    
+    result = cur.execute(sql, (startLimit,21))    
     rowcount = result.fetchall()
     disableNext = 1
     data = []
@@ -231,7 +214,7 @@ def paging(page, sql, template):
     elif int(page) < 1:
         return render_template('404.html'), 404
     else:
-        for item in cur.execute(sql.format(startLimit, 20)):
+        for item in cur.execute(sql, (startLimit, 20)):
             data.append(item)
         if len(rowcount) > 20:
             disableNext = 0
@@ -242,11 +225,11 @@ def paging(page, sql, template):
 def categoriesCategory(category, page):
     db = get_db() 
     cur = db.cursor()  
-    sql = "SELECT COUNT(*) FROM categories WHERE category_name = '" + category + "'"
-    result = cur.execute(sql)
+    sql = "SELECT COUNT(*) FROM categories WHERE category_name = ?"
+    result = cur.execute(sql, (category, ))
     rowcount = result.fetchall()
     if rowcount[0][0] > 0:
-        sql = "SELECT quiz_id, c.category_name, u.username, quiz_name, plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id WHERE c.category_name = '" + category + "' LIMIT {}, {}"
+        sql = "SELECT quiz_id, c.category_name, u.username, quiz_name, plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id WHERE c.category_name = '" + category + "' LIMIT ?, ?"
         template = 'category.html'
         return paging(page, sql, template) 
     else:
@@ -255,21 +238,21 @@ def categoriesCategory(category, page):
 @app.route('/popular/<page>')
 @login_required
 def popular(page):  
-    sql = "SELECT q.quiz_id, c.category_name, u.username, q.quiz_name, q.plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id ORDER BY q.plays DESC LIMIT {}, {}"
+    sql = "SELECT q.quiz_id, c.category_name, u.username, q.quiz_name, q.plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id ORDER BY q.plays DESC LIMIT ?, ?"
     template = 'popularQuizzes.html'
     return paging(page, sql, template)
 
 @app.route('/myQuizzes/<page>')
 @login_required
 def myQuizzes(page):    
-    sql = "SELECT q.quiz_id, c.category_name, u.username, q.quiz_name, q.plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id WHERE q.user_id = " + str(current_user.id) + " LIMIT {}, {}"
+    sql = "SELECT q.quiz_id, c.category_name, u.username, q.quiz_name, q.plays FROM quizzes q JOIN user u ON q.user_id = u.id JOIN categories c ON q.category_id = c.category_id WHERE q.user_id = " + str(current_user.id) + " LIMIT ?, ?"
     template = 'myQuizzes.html'
     return paging(page, sql, template)
 
 @app.route('/leaderboard/<page>')
 @login_required
 def leaderboard(page):
-    sql = "SELECT u.id, u.username, u.played_quizzes + (u.challenge_score * 10) + ((SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = u.id) * 15) as score, (SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = u.id) as numberOfAchievements, u.played_quizzes, u.challenge_score FROM user u ORDER BY score DESC LIMIT {}, {}"
+    sql = "SELECT u.id, u.username, u.played_quizzes + (u.challenge_score * 10) + ((SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = u.id) * 15) as score, (SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = u.id) as numberOfAchievements, u.played_quizzes, u.challenge_score FROM user u ORDER BY score DESC LIMIT ?, ?"
     template = 'leaderboard.html'
     return paging(page, sql, template)
 
@@ -343,8 +326,7 @@ def _saveQuizToDB():
                 categoryExists = True
                 break
         if not categoryExists:
-            raise Exception("Category does not exist")
-        
+            raise Exception("Category does not exist")        
         #number of questions cannot be empty, has to be a number, and be bigger than 1
         if not f['numberOfQuestions']:
             raise Exception("Number of questions was zero")
@@ -352,11 +334,9 @@ def _saveQuizToDB():
             raise Exception("Number of questions was not a number")
         elif int(f['numberOfQuestions']) < 1:
             raise Exception("There were no questions")
-
-        sql = "INSERT INTO quizzes (category_id, user_id, quiz_name, plays) VALUES ({}, '{}', '{}', 0)"
-        cursor.execute(sql.format(f['selectCategory'], current_user.id, f['quizName']))        
+        sql = "INSERT INTO quizzes (category_id, user_id, quiz_name, plays) VALUES (?, ?, ?, 0)"
+        cursor.execute(sql, (f['selectCategory'], current_user.id, f['quizName']))        
         quiz_id = cursor.lastrowid 
-
         questionsToSave = []        
         for i in range(int(f['numberOfQuestions'])):            
             questionQ = []
@@ -373,7 +353,6 @@ def _saveQuizToDB():
                 questionQ.append(f['answer2' + str(i + 1)])
                 questionQ.append(f['answer3' + str(i + 1)])
                 questionQ.append(f['answer4' + str(i + 1)])
-
             file = request.files['image' + str(i + 1)]
             if not file.filename:
                 questionQ.append('/static/questionImages/noImage.PNG')
@@ -388,12 +367,10 @@ def _saveQuizToDB():
                     questionQ.append('/static/questionImages/' + uniqueFilename)
                 else:
                     raise Exception("Wrong file format")
-
             if f['correctAnswer' + str(i + 1)] == "answer1" or f['correctAnswer' + str(i + 1)] == "answer2" or f['correctAnswer' + str(i + 1)] == "answer3" or f['correctAnswer' + str(i + 1)] == "answer4":
                 questionQ.append(f['correctAnswer' + str(i + 1)])                 
             else:                
                 raise Exception("No correct answer selected")
-
             if not f['timeLimit' + str(i + 1)]:
                 raise Exception("Time limit was empty")
             elif not f['timeLimit' + str(i + 1)].isdigit():
@@ -402,7 +379,6 @@ def _saveQuizToDB():
                 raise Exception("Time limit was not in accepted range")
             else:
                 questionQ.append(f['timeLimit' + str(i + 1)])
-
             questionQ.append('0')
             questionQ.append('0')
             questionsToSave.append(questionQ)      
@@ -424,10 +400,10 @@ def _updatedb():
     for item in whatToUpdate:
         page.append(item + " " + whatToUpdate[item] + "<br>")
     for i in range(int(whatToUpdate['numberOfQuestions'])):
-        sql = "UPDATE questions SET attempts = attempts + 1, correct_attempts = correct_attempts + " + whatToUpdate['questions[' + str(i) + '][correct_attempts]'] + " WHERE question_id = " + whatToUpdate['questions[' + str(i) + '][question_id]'] + ""
-        db.cursor().execute(sql)    
-    sql = "UPDATE user SET played_quizzes = played_quizzes + 1 WHERE username = '" + current_user.username + "'"
-    db.cursor().execute(sql)
+        sql = "UPDATE questions SET attempts = attempts + 1, correct_attempts = correct_attempts + ? WHERE question_id = ?"
+        db.cursor().execute(sql, (whatToUpdate['questions[' + str(i) + '][correct_attempts]'], whatToUpdate['questions[' + str(i) + '][question_id]']))    
+    sql = "UPDATE user SET played_quizzes = played_quizzes + 1 WHERE username = ?"
+    db.cursor().execute(sql, (current_user.username, ))
     db.commit()
     return jsonify(result=page)
 
@@ -435,15 +411,16 @@ def _updatedb():
 @login_required
 def _updateChallengedb():    
     db = get_db()
-    whatToUpdate = request.args    
+    whatToUpdate = request.args
+    print(whatToUpdate)
     page = []
     for item in whatToUpdate:
-        page.append(item + " " + whatToUpdate[item] + "<br>")
+        page.append(item + " " + whatToUpdate[item] + "<br>")    
     for i in range(int(whatToUpdate['numberOfQuestions'])):
-        sql = "UPDATE questions SET attempts = attempts + 1, correct_attempts = correct_attempts + " + whatToUpdate['questions[' + str(i) + '][correct_attempts]'] + " WHERE question_id = " + whatToUpdate['questions[' + str(i) + '][question_id]'] + ""
-        db.cursor().execute(sql)    
-    sql = "UPDATE user SET challenge_score = challenge_score + {} WHERE username = '" + current_user.username + "'"
-    db.cursor().execute(sql.format(whatToUpdate['correctNumber']))
+        sql = "UPDATE questions SET attempts = attempts + 1, correct_attempts = correct_attempts + ? WHERE question_id = ?"
+        db.cursor().execute(sql, (whatToUpdate['questions[' + str(i) + '][correct_attempts]'], whatToUpdate['questions[' + str(i) + '][question_id]']))    
+    sql = "UPDATE user SET challenge_score = challenge_score + ? WHERE username = ?"
+    db.cursor().execute(sql, (whatToUpdate['correctNumber'], current_user.username))
     db.commit()
     return jsonify(result=page)
 
@@ -458,15 +435,15 @@ def profile(id):
         userid = id
         remove = 1    
     cur = db.cursor()  
-    sql = "SELECT COUNT(*) FROM user WHERE id = " + str(userid) + ""
-    result = cur.execute(sql)
+    sql = "SELECT COUNT(*) FROM user WHERE id = ?"
+    result = cur.execute(sql, (userid, ))
     rowcount = result.fetchall()
     print(rowcount)
     if rowcount[0][0] > 0:
-        sql = "SELECT u.username, u.played_quizzes, u.profile_pic, u.introduction, u.reg_date, (SELECT COUNT(*) FROM quizzes q WHERE q.user_id = {}) as created_quizzes, u.challenge_score, u.played_quizzes + (u.challenge_score * 10) + ((SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = {}) * 15) as score FROM user u WHERE u.id = {}"
-        userdata = db.cursor().execute(sql.format(userid, userid, userid)).fetchall()       
-        sql = "SELECT a.achievement_name, a.description, a.icon FROM achievements a JOIN earned_achievements e ON a.achievement_id = e.achievement_id WHERE e.user_id = {}"
-        userachievements = db.cursor().execute(sql.format(userid)).fetchall()
+        sql = "SELECT u.username, u.played_quizzes, u.profile_pic, u.introduction, u.reg_date, (SELECT COUNT(*) FROM quizzes q WHERE q.user_id = ?) as created_quizzes, u.challenge_score, u.played_quizzes + (u.challenge_score * 10) + ((SELECT COUNT(*) FROM earned_achievements ea WHERE ea.user_id = ?) * 15) as score FROM user u WHERE u.id = ?"
+        userdata = db.cursor().execute(sql, (userid, userid, userid)).fetchall()       
+        sql = "SELECT a.achievement_name, a.description, a.icon FROM achievements a JOIN earned_achievements e ON a.achievement_id = e.achievement_id WHERE e.user_id = ?"
+        userachievements = db.cursor().execute(sql, (userid, )).fetchall()
         return render_template('profile.html', name=current_user.username, userdata=userdata, userachievements=userachievements, remove=remove)
     else:
         return render_template('404.html'), 404
@@ -475,13 +452,14 @@ def profile(id):
 @login_required
 def editProfile():
     db = get_db()
-    sql = "SELECT u.username, u.profile_pic, u.introduction FROM user u WHERE u.id = {}"
-    userdata = db.cursor().execute(sql.format(current_user.id)).fetchall()
+    sql = "SELECT u.username, u.profile_pic, u.introduction FROM user u WHERE u.id = ?"
+    userdata = db.cursor().execute(sql, (current_user.id, )).fetchall()
     return render_template('editProfile.html', name=current_user.username, userdata=userdata)
 
 @app.route('/_saveUserdataToDB', methods=['GET', 'POST'])
 @login_required
 def _saveUserdataToDB(): 
+    print(sqlite3.paramstyle)
     f = request.form
     try:                 
         db = get_db()
@@ -498,15 +476,15 @@ def _saveUserdataToDB():
                 uniqueFilename = uuid.uuid4().hex + filename
                 image.save(os.path.join(app.config['UPLOAD_FOLDER2'], uniqueFilename))
                 filePath = '/static/profilePictures/' + uniqueFilename
-                sql = "UPDATE user SET profile_pic = '{}' WHERE id = {}"
-                cursor.execute(sql.format(filePath, current_user.id))    
+                sql = "UPDATE user SET profile_pic = ? WHERE id = ?"   
+                cursor.execute(sql, (filePath, current_user.id))
                 db.commit()
             else:
                 raise Exception("Wrong file format")
         elif f['introduction'] and not file.filename:
             introduction = f['introduction']
-            sql = "UPDATE user SET introduction = '{}' WHERE id = {}"
-            cursor.execute(sql.format(introduction, current_user.id))    
+            sql = "UPDATE user SET introduction = ? WHERE id = ?"
+            cursor.execute(sql, (introduction, current_user.id))    
             db.commit()
         else:
             introduction = f['introduction']
@@ -518,8 +496,8 @@ def _saveUserdataToDB():
                 uniqueFilename = uuid.uuid4().hex + filename
                 image.save(os.path.join(app.config['UPLOAD_FOLDER2'], uniqueFilename))
                 filePath = '/static/profilePictures/' + uniqueFilename
-                sql = "UPDATE user SET profile_pic = '{}', introduction = '{}' WHERE id = {}"
-                cursor.execute(sql.format(filePath, introduction, current_user.id))    
+                sql = "UPDATE user SET profile_pic = ?, introduction = ? WHERE id = ?"
+                cursor.execute(sql, (filePath, introduction, current_user.id)) 
                 db.commit()
             else:
                 raise Exception("Wrong file format")
@@ -532,8 +510,8 @@ def _saveUserdataToDB():
 def dailyChallenge():
     db = get_db() 
     cur = db.cursor()  
-    sql = "SELECT COUNT(user_id) FROM challenge_progress WHERE user_id={}"
-    result = cur.execute(sql.format(current_user.id))    
+    sql = "SELECT COUNT(user_id) FROM challenge_progress WHERE user_id = ?"
+    result = cur.execute(sql, (current_user.id, ))    
     rowcount = result.fetchone()[0]
     if rowcount > 0:
         return render_template('dailyChallenge.html', questions="empty", alreadyAttempted=rowcount, name=current_user.username)
@@ -548,8 +526,8 @@ def dailyChallenge():
 @login_required
 def _addUserToChallengeProgress():
     db = get_db() 
-    sql = "INSERT INTO challenge_progress (user_id, challenge_progress) VALUES ({},{})"
-    db.cursor().execute(sql.format(current_user.id, 1))
+    sql = "INSERT INTO challenge_progress (user_id, challenge_progress) VALUES (?,?)"
+    db.cursor().execute(sql, (current_user.id, 1))
     db.commit() 
     return jsonify("added")
 
@@ -622,4 +600,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.72', port=3232, debug=True)
+    app.run(host='127.0.0.1', port=3232, debug=True)
